3b54ef2e20d2d495938a3b2c193bfdd8accd2a4a
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-07-18 14:09:27 -0300

    lambda expression kotlin

---
+++ Unit-2-How-To-Create-The-App-Interface/1-Kotlin-basics/src/main/lambda/LambdaExample.kt
@@ -0,0 +1,29 @@
+package main.lambda
+
+fun main() {
+    val treatFunction = trickOrTreat(false) { "$it quarters" }
+    val trickFunction = trickOrTreat(true, null)
+    repeat(4) {
+        treatFunction()
+    }
+    trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    return if (isTrick) {
+        trick
+    } else {
+        if (extraTreat != null) {
+            println(extraTreat(5))
+        }
+        treat
+    }
+}
+
+val trick = {
+    println("No treats!")
+}
+
+val treat: () -> Unit = {
+    println("Have a treat!")
+}
