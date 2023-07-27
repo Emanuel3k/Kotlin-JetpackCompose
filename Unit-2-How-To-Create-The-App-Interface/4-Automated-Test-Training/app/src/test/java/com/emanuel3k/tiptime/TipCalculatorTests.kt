d82ba5adfbfb146f91b50f8af64bb62f5f3b1d94
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-07-27 14:18:38 -0300

    Automated Test TIpTIme app

---
+++ Unit-2-How-To-Create-The-App-Interface/4-Automated-Test-Training/app/src/test/java/com/emanuel3k/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,21 @@
+package com.emanuel3k.tiptime
+
+import com.example.tiptime.calculateTip
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculate_20_percent_tip_no_roundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+
+    }
+
+
+}
